c9ddfdb5aa2cb7371f07c78b11a55b4c85dda3b4
kvares <johndoe@example.com>
2023-10-02 12:32:34 +0300

    first

---
+++ kahoot.py
@@ -0,0 +1,13 @@
+from PIL import Image
+
+import pytesseract
+
+import numpy as np
+
+filename = 'hanna.jpg'
+
+img1 = np.array(Image.open(filename))
+
+text = pytesseract.image_to_string(img1)
+
+print(text)
